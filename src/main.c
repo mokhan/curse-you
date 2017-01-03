c1b827d1210405f309fd9bac3275f536beea282d
mo khan <mo@mokhan.ca>
2017-01-03 12:24:57 -0700

    add printw example.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 
 void hello_world() {
   initscr();
@@ -8,8 +9,22 @@ void hello_world() {
   endwin();
 }
 
+void printw_example(){
+  char mesg[]="Just a string";
+  int row,col;
+  initscr();
+  getmaxyx(stdscr,row,col);
+  mvprintw(row/2, (col-strlen(mesg))/2, "%s", mesg);
+  mvprintw(row-2, 0, "this screen has %d rows and %d columns\n",row,col);
+  printw("Try resizing your window (if possible) and then run this program again");
+  refresh();
+  getch();
+  endwin();
+}
+
 int main(int argc, const char *argv[])
 {
   hello_world();
+  printw_example();
   return 0;
 }
